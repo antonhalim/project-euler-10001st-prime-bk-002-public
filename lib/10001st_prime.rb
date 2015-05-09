e12a0534cc56ff00628483049e613cdc4c45d791
axton21 <axton_n@yahoo.com>
2015-05-09 17:44:41 -0400

    done

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,15 @@
-# Implement your procedural solution here!
+def prime_number_for(index) #2 --> 3;  4 --> 7
+	array = [2]
+	possible_prime = 2
+	until array.length == index do
+		isPrime = true
+		array.each do |prime|
+			if possible_prime % prime == 0
+				isPrime = false
+			end
+		end
+		array << possible_prime if isPrime == true
+		possible_prime += 1
+	end
+return array.last
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,26 @@
-# Implement your object-oriented solution here!
+class Prime
+	attr_reader :index
+
+	def initialize(index)
+		@index = index
+	end
+
+	def number
+			array = [2]
+			possible_prime = 2
+			until array.length == index do
+				isPrime = true
+				array.each do |prime|
+					if possible_prime % prime == 0
+						isPrime = false
+					end
+				end
+				array << possible_prime if isPrime == true
+				possible_prime += 1
+			end
+		return array.last
+	end
+
+
+
+end
